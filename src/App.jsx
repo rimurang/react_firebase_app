718fb1abb28b0fdb7ccaf81aeadcf7f732445be3
rimurang <dkffkqb000@gmail.com>
2025-09-16 13:15:19 +0900

    JSX와 컴포넌트 기초

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,22 @@
-import { useState } from "react";
+import Greeting from "./Greeting"; // 컴포넌트 불러오기
+
 import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(0);
+  const name = "나나";
+  const isLogin = true;
 
   return (
-    <>
-      <p>Hello React </p>
-    </>
+    <div>
+      <h3>JSX 기본 문법 </h3>
+      {/* 중괄호  {} 안에 JS표현식 사용 가능 */}
+      <p>Hello, {name}</p>
+      <p>{isLogin ? "로그인 성공" : "로그인 필요"}</p>
+
+      <h3>Greeting 컴포넌트 예제 (props)</h3>
+      <Greeting name="철수" />
+      <Greeting name="영희" />
+    </div>
   );
 }
 
---
+++ src/Greeting.jsx
@@ -0,0 +1,10 @@
+function Greeting({ name }) {
+  return (
+    <div>
+      {/* props 전달하기 */}
+      <p>안녕, {name}</p>
+    </div>
+  );
+}
+
+export default Greeting;
